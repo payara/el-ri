4e8a3e0d5d512523509f66a89bfeb2a622fd3e42
merorai <susan.rai04@gmail.com>
2020-05-15 12:20:59 +0100

    CUSTCOM-273 PrimeFaces Events are Null within Custom Component

--- impl/src/main/java/com/sun/el/util/ReflectionUtil.java
+++ impl/src/main/java/com/sun/el/util/ReflectionUtil.java
@@ -149,8 +149,11 @@ public class ReflectionUtil {
         try {
             int paramCount = params == null ? 0 : params.length;
             if (paramCount == 0) {
-                for (Class ParameterType : method.getParameterTypes()) {
-                    if (ParameterType.getName().equals("javax.faces.event.AjaxBehaviorEvent")) {
+                for (Class parameterType : method.getParameterTypes()) {
+                    String parameterTypeName = parameterType.getName();
+                    if (parameterTypeName.matches("javax.faces.event.*")
+                            || parameterTypeName.matches("org.primefaces.event.*")
+                            || parameterTypeName.matches("org.primefaces.extensions.event.*")) {
                         return method.invoke(base, (Object[]) params);
                     }
                 }
